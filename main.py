fae9b58a62e882aad99cf37e06e231c5aae93d53
Jhoope11 <jhoope11@kent.edu>
2023-12-06 14:54:08 -0500

    .

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import csv
-import pandas
 import math
 
 def performCalc(event):
-    with open('Type12.csv') as csvFile:
+    with open('./Type12.csv') as csvFile:
         csvReader= csv.reader(csvFile,delimiter=',')
         lineCount=0
         PercentType1 = 0
@@ -11,7 +10,7 @@ def performCalc(event):
         PercentType0 = 0
         for row in csvReader:
             if lineCount == 0:
-                print(f'Col names are{",".join(row)} {row}')
+                print(f'Col names are {", ".join(row)} ')
                 lineCount+=1
             else:
                 y = float(row[0])
@@ -27,4 +26,7 @@ def performCalc(event):
         PercentType0 = PercentType0 / x * 100
         PercentType1 = PercentType1 / x * 100
         PercentType2 = PercentType2 / x * 100
-        print(f'processed {lineCount} lines. \n {PercentType0} are not diabetic. \n {PercentType1} Have type1 \n {PercentType2} Have Type 2')
+        print(f'processed {lineCount} lines. \n{PercentType0} are not diabetic. \n{PercentType1} Have are type 1 \n{PercentType2} Have are type 2')
+        
+######################################MAIN#######################################
+#performCalc()
