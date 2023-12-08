800e3bc6a0c4cfb21add5b961d1706b2b7c1ac25
Jhoope11 <jhoope11@kent.edu>
2023-12-08 16:02:19 -0500

    .

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 import csv
 import math
+import pandas as pd
 #from pyscript import document
+
+def sameDataType():
+    df = pd.read_csv('Type12 copy.csv')
+    df['Diabetes_012'] = pd.to_numeric(df['Diabetes_012'])
+    
+
+
 
 def performCalc():
     HighBP = 1
@@ -32,10 +40,12 @@ def performCalc():
         PercentType1 = 0
         PercentType2 = 0
         PercentType0 = 0
+        matchedCol = 0
         matchedRow = 0
         for row in csvReader:
             if lineCount == 0:
                 print(f'Col names are {", ".join(row)} ')
+                print(EnteredInfo)
                 lineCount+=1
             else:
                 y = float(row[0])
@@ -46,13 +56,22 @@ def performCalc():
                 else:
                     PercentType0 += 1
                 lineCount +=1
-                rowInfo = [row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21]]
-                #print(rowInfo)
-        if {[i for i, j in zip(EnteredInfo, rowInfo) if i == j]}:
-            matchedRow += 1
-        print(f'{[i for i, j in zip(EnteredInfo, rowInfo) if i == j]}')
+                rowInfo = [row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14], row[15], row[16], row[17], row[18], row[19], row[20], row[21]] 
+                for i in EnteredInfo:
+                    result1 = type(EnteredInfo[i])
+                    result2 = type(rowInfo[i])
+                    print(result1,result2)
+                    if EnteredInfo[i] == rowInfo[i]:
+                        #print(EnteredInfo[i], rowInfo[i])
+                        matchedCol += 1
+                        #print(f"col match")
+                        if matchedCol == 21:
+                            print(f"RowMatch")
+                            matchedRow += 1
+
             
         x = lineCount - 1
+        print(f'This has {matchedRow} rows That are the same')
         print(f'{PercentType0} Are not diabetic \n{PercentType1} Have Type 1 \n{PercentType2} Have Type 2')
         PercentType0 = PercentType0 / x * 100
         PercentType1 = PercentType1 / x * 100
@@ -61,4 +80,4 @@ def performCalc():
         #outputDiv= document.querySelector("#output")
         #outputDiv.innerText = 'processed {lineCount} lines. \n{PercentType0} are not diabetic. \n{PercentType1} Have are type 1 \n{PercentType2} Have are type 2'
 ######################################MAIN#######################################
-performCalc()
+sameDataType()
