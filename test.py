35d5afca0bee18625c13b36eeda2a5aea9679db6
Jhoope11 <jhoope11@kent.edu>
2023-12-08 17:09:02 -0500

    It works

--- test.py
+++ test.py
@@ -2,14 +2,6 @@ import pandas as pd
 import numpy as np
 #from pyscript import document
 
-def sameDataType():
-    df = pd.read_csv('Type12 copy.csv')
-    df['HighChol'] = pd.to_numeric(df['HighChol'])
-    print(df.info())
-    
-
-
-
 def performCalc():
     enteredData = {
     'Diabetes_012': 0.0,  # Assuming a default value of 0.0 for Diabetes_012
@@ -36,20 +28,22 @@ def performCalc():
     'Income': 1.0  # Assuming a default value of 1.0 for Income
 }
 
-    csv_file_path = './Type12 copy.csv'
+    csv_file_path = './Type12.csv'
     df = pd.read_csv(csv_file_path)
     enteredDf = pd.DataFrame([enteredData])
+    #ensures column names are the same
     dfCols = df.columns
+    #Reindexes enteredDf so the data types are the same
     enteredDf = enteredDf.reindex(columns=dfCols, fill_value=None)
-    
+    #Resets index
     df = df.reset_index(drop=True)
     enteredDf = enteredDf.reset_index(drop=True)
-    
-
-    
-    print(df.info())
-    print(enteredDf.info())
+    #Print functions to check data types and column names are correct
+    #print(df.info())
+    #print(enteredDf.info())
+    #Locks just the first row as a var since thats what needs to be compared
     firstRowEnteredDf = enteredDf.iloc[0]
+    #Does the comparison
     comparisonResult = (df == firstRowEnteredDf)
         
     colMatch = comparisonResult.all()
