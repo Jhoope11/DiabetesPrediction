80416e44b9a80245af50170744f2fb92e37aea8e
Jhoope11 <jhoope11@kent.edu>
2023-12-15 17:39:56 -0500

    attempt

--- script.js
+++ script.js
@@ -9,108 +9,112 @@ async function parseCSV(csvFile) {
         const values = line.split(',').map(value => parseFloat(value.trim()));
         return Object.fromEntries(headers.map((header, index) => [header, values[index]]));
     });
-
+    console.log(headers, data);
     return { headers, data };
 }
 
-// Plot a bar chart using Plotly
-function plotBarChart(barChart, data, xAxisValues, yAxisColumn) {
-    const headers = Object.keys(data[0]);
-
-    // Filter out rows with negative values in the specified columns
-    function filterData(column) {
-        return data.filter(entry => entry[column] >= 0);
-    }
-
-    // Count occurrences of user input in each column
-    const userCounts = headers.map(column => {
-        const count = filterData(column).filter(entry => {
-            const entryValue = entry[yAxisColumn];
-            return xAxisValues.includes(entryValue);
-        }).length;
-        return count;
-    });
-
-    // Create bars for CSV data
-    const csvBars = headers.flatMap((column, index) => {
-        const csvData = filterData(column);
-        return [
-            {
-                x: xAxisValues,
-                y: Array(xAxisValues.length).fill(0), // All bars for CSV data have y=0
-                type: 'bar',
-                name: `${column} (CSV Data)`,
-                showlegend: false,
-                marker: { color: 'blue' },
+// Function to filter data based on user input
+function filterData(data, headers, formData) {
+    return data.filter(entry => {
+        return headers.filter(header => {
+            const userInputValue = formData[header];
+            const entryValue = entry[header];
+            //console.log('Header:', header, 'User Input Value:', userInputValue, 'Entry Value:', entryValue);
+            if (typeof userInputValue === 'boolean') {
+                // For checkboxes
+                return userInputValue ? entryValue === 1.0 : entryValue === 0.0;
+            } else {
+                // For text entries (convert empty to 0.0)
+                const numericUserInput = parseFloat(userInputValue);
+                return (
+                    entryValue !== undefined &&
+                    !isNaN(numericUserInput) &&
+                    entryValue === (userInputValue === '' ? 0.0 : numericUserInput)
+                );
             }
-        ];
-    });
-
-    // Create bars for user input counts
-    const userInputBars = headers.map((column, index) => {
-        return {
-            x: xAxisValues,
-            y: userCounts,
-            type: 'bar',
-            name: `${column} (User Input)`,
-            showlegend: false,
-            marker: { color: 'red' },
-        };
-    });
-
-    // Plotly Bar Chart
-    Plotly.newPlot(barChart, csvBars.concat(userInputBars), {
-        barmode: 'group',
-        margin: { t: 40, r: 10, b: 80, l: 60 },
-        xaxis: {
-            title: 'User Input',
-            ticktext: headers,
-            tickmode: 'array',
-            tickvals: headers.map((_, index) => index),
-        },
-        yaxis: { title: 'Number of Matches' }, // Update Y-axis label
+        });
     });
 }
 
+
+// Plot a bar chart using Plotly
+function plotBarChart(barChart, data, formData) {
+    const headers = Object.keys(data[0]);
+
+    // Filter data based on user input
+    const filteredData = filterData(data, headers, formData);
+
+    // Create a map to store match counts for each header
+    const userCounts = new Map();
+
+    // Iterate through the filtered data to count matches and create bars
+    const userInputBars = filteredData.reduce((bars, entry) => {
+        headers.forEach(header => {
+            const userInputValue = parseFloat(formData[header]);
+            const entryValue = entry[header];
+            // For numeric values
+            if (entryValue === userInputValue) {
+                // Increment the match count for the current header
+                userCounts.set(header, (userCounts.get(header) || 0) + 1);
+                // Create a new bar for the current match
+                const matchCount = userCounts.get(header);
+                const yValue = matchCount;
+                bars.push({
+                    x: [header],
+                    y: [yValue],
+                    type: 'bar',
+                    name: `${header} (User Input)`,
+                    showlegend: false,
+                    marker: { color: 'red' },
+                });
+            }
+        });
+        return bars;
+    }, []);
+    // Plotly Bar Chart
+    Plotly.newPlot(barChart, userInputBars, {
+        barmode: 'group',
+        margin: { t: 40, r: 10, b: 80, l: 60 },
+        xaxis: { title: 'User Input' },
+        yaxis: { title: 'Incrementing Count' },
+    });
+}
 // Function to submit the form and process user input
 async function submitForm(event) {
     event.preventDefault();
 
     // Get user input values from the form
     const formData = {
-        HighBP: document.getElementById('HighBP').checked,
-        HighChol: document.getElementById('HighChol').checked,
-        CholCheck: document.getElementById('CholCheck').checked,
-        Smoke: document.getElementById('Smoke').checked,
-        Stroke: document.getElementById('Stroke').checked,
-        HeartDiseaseOrAttack: document.getElementById('HeartDiseaseOrAttack').checked,
-        PhysAct: document.getElementById('PhysAct').checked,
-        Fruit: document.getElementById('Fruit').checked,
-        Veg: document.getElementById('Veg').checked,
-        HvyAlcCons: document.getElementById('HvyAlcCons').checked,
-        Healthcare: document.getElementById('Healthcare').checked,
-        NoDocCost: document.getElementById('NoDocCost').checked,
-        DiffWalk: document.getElementById('DiffWalk').checked,
-        BMI: document.getElementById('BMI').value,
-        genHealth: document.getElementById('genHealth').value,
-        mentalHealth: document.getElementById('mentalHealth').value,
-        physHealth: document.getElementById('physHealth').value,
-        gender: document.getElementById('gender').value,
-        age: document.getElementById('age').value,
-        eduLevel: document.getElementById('eduLevel').value,
-        income: document.getElementById('income').value,
+        HighBP: document.getElementById('HighBP').checked ? 1 : 0,
+        HighChol: document.getElementById('HighChol').checked ? 1.0 : 0.0,
+        CholCheck: document.getElementById('CholCheck').checked ? 1.0 : 0.0,
+        Smoker: document.getElementById('Smoke').checked ? 1.0 : 0.0,
+        Stroke: document.getElementById('Stroke').checked ? 0.0 : 1.0,
+        HeartDiseaseorAttack: document.getElementById('HeartDiseaseOrAttack').checked ? 1.0 : 0.0,
+        PhysActivity: document.getElementById('PhysAct').checked ? 1 : 0,
+        Fruits: document.getElementById('Fruit').checked ? 1 : 0,
+        Veggies: document.getElementById('Veg').checked ? 1 : 0,
+        HvyAlcoholConsump: document.getElementById('HvyAlcCons').checked ? 1 : 0,
+        AnyHealthcare: document.getElementById('Healthcare').checked ? 1 : 0,
+        NoDocbcCost: document.getElementById('NoDocCost').checked ? 1.0 : 0.0,
+        DiffWalk: document.getElementById('DiffWalk').checked ? 1.0 : 0.0,
+        BMI: document.getElementById('BMI').value || 0.0,
+        genHealth: document.getElementById('genHealth').value || 0.0,
+        mentalHealth: document.getElementById('mentalHealth').value || 0.0,
+        physHealth: document.getElementById('physHealth').value || 0.0,
+        gender: document.getElementById('gender').value || 0.0,
+        age: document.getElementById('age').value || 0.0,
+        eduLevel: document.getElementById('eduLevel').value || 0.0,
+        income: document.getElementById('income').value || 0.0,
     };
 
     // Parse CSV data from the external file
     const csvFile = 'Type12.csv'; // Replace with the actual path to your CSV file
     const parsedData = await parseCSV(csvFile);
 
-    // Extract X-axis values from the user input
-    const xAxisValues = Object.entries(formData)
-        .filter(([key, value]) => key !== 'BMI' && key !== 'genHealth' && key !== 'mentalHealth' && key !== 'physHealth')
-        .filter(([key, value]) => value)
-        .map(([key, value]) => value);
-
     // Plot the bar chart
-    plotBarChart('barChart', parsedData.data, xAxisValues, 'BMI'); // 'BMI' is the Y-axis column
+    plotBarChart('barChart', parsedData.data, formData);
 }
+
+// Initial plot when the page loads
+submitForm({ preventDefault: () => {} });
